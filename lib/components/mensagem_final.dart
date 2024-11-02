8381595e3f10741cb79fc0843df7c858db550d72
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-11-02 19:34:45 -0300

    refactor: desacoplamento de widget 'mensagem final'

---
+++ lib/components/mensagem_final.dart
@@ -0,0 +1,21 @@
+﻿import 'package:flutter/material.dart';
+
+class MensagemFinal extends StatelessWidget {
+  final String mensagemFinal;
+  const MensagemFinal({super.key, required this.mensagemFinal});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Text(
+        mensagemFinal,
+        style: const TextStyle(
+          color: Colors.green,
+          fontSize: 32,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
