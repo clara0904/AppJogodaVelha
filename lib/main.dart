2bd4920ffd34afa0cf245b33cdb85024ba48782d
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-11-02 19:22:56 -0300

    feat: adição da tela de jogo ao home

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         primaryColor: const Color.fromARGB(255, 12, 18, 49),
         scaffoldBackgroundColor: const Color.fromARGB(255, 12, 18, 49)
