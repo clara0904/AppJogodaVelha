7e9b29b05eed95912f7f926c57d144311c18aa88
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-11-02 19:35:16 -0300

    refactor: desacoplamento de widget 'jogada'

---
+++ lib/components/jogada.dart
@@ -0,0 +1,21 @@
+﻿import 'package:flutter/material.dart';
+
+class Jogada extends StatelessWidget {
+  final String jogadorAtual;
+  const Jogada({super.key, required this.jogadorAtual});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Text(
+        'Faça a sua jogada, jogador $jogadorAtual!',
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 32,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+}
