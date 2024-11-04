6790103183f907ec3b0e8b9f2d3c30925a73f8f7
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-11-04 19:46:14 -0300

    refactor: desacoplamento de widget 'botao reiniciar'

---
+++ lib/components/botao_reiniciar.dart
@@ -0,0 +1,22 @@
+﻿import 'package:flutter/material.dart';
+
+class ButtonReiniciar extends StatelessWidget {
+  final Function onPressed;
+  const ButtonReiniciar({super.key, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: const ButtonStyle(backgroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 14, 8, 146))),
+      onPressed: () {
+        onPressed();
+      },
+      child: const Text("Reiniciar Jogo", 
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+        ),
+      )
+    );
+  }
+}
