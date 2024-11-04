d8691b69d6188b76787c8a15389a8e031ae50918
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-11-04 19:46:52 -0300

    refactor: desacoplamento de widget 'container jogo'

---
+++ lib/components/container_jogo.dart
@@ -0,0 +1,22 @@
+﻿import 'package:flutter/material.dart';
+
+class ContainerJogo extends StatelessWidget {
+  final Widget Function(int) box;
+  const ContainerJogo({super.key, required this.box});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height/2,
+      width: MediaQuery.of(context).size.height/2,
+      margin: const EdgeInsets.all(8),
+      child: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+        itemCount: 9, 
+        itemBuilder: (context, int index){
+          return box(index);
+        }
+      ),
+    );
+  }
+}
