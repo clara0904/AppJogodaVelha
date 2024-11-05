972e0fb00774fdd7fd17d816426e879b842a33a7
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-11-04 21:22:30 -0300

    feat: criação da classe 'game_result'

---
+++ lib/drift/game_result.dart
@@ -0,0 +1,13 @@
+﻿/*import 'package:drift/drift.dart';
+
+part 'database.g.dart';
+
+class GameResults extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get vencedor => text().nullable()(); 
+  DateTimeColumn get data => dateTime().withDefault(currentDateAndTime)();
+}
+
+@DriftDatabase(tables: [GameResults])
+class AppDatabase extends _$AppDatabase {
+}*/
