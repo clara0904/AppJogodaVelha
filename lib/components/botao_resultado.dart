2255181c875b6379bd10fd4e309a13daccca82e5
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-11-04 21:22:02 -0300

    feat: criação do widget 'botao resultado'

---
+++ lib/components/botao_resultado.dart
@@ -0,0 +1,22 @@
+﻿import 'package:flutter/material.dart';
+
+class BotaoResultado extends StatelessWidget {
+  final Function onPressed;
+  const BotaoResultado({super.key, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: const ButtonStyle(backgroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 10, 114, 114))),
+      onPressed: () {
+        onPressed();
+      },
+      child: const Text("Ver resultados", 
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
+}
